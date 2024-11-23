f8d4df4d0fc9a88c9db412e6713bd147359b7e82
Hans Dussich <hansdussich2004@gmail.com>
2024-11-23 11:42:25 -0500

    Creating the proyect and add dependencies

---
+++ src/main/java/com/example/testSymplifica/TestSymplificaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testSymplifica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSymplificaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSymplificaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testSymplifica/TestSymplificaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testSymplifica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSymplificaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
